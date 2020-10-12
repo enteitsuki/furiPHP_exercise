b487a70732100a2e72e8afd4479a6abec99f851d
enteitsuki <enteitsuki@gmail.com>
2020-10-13 00:00:39 +0900

    Chapter1課題 半角スペースとtrim関数修正

--- chap1_2.php
+++ chap1_2.php
@@ -1,8 +1,8 @@
 <?
 var_dump("上底は？");
-$top = fgets(STDIN);
+$top = trim(fgets(STDIN));
 var_dump("下底は？");
-$bottom = fgets(STDIN);
+$bottom = trim(fgets(STDIN));
 var_dump("高さは？");
-$height = fgets(STDIN);
-var_dump("面積は" . ($top + $bottom)* $height / 2);
+$height = trim(fgets(STDIN));
+var_dump("面積は" . ($top + $bottom) * $height / 2);
